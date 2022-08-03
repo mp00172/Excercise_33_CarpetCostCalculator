144b5eda88ac47eea32be74202ab8e68b75d6cb6
mp00172 <61931347+mp00172@users.noreply.github.com>
2022-08-03 15:32:08 +0200

    Add files via upload

---
+++ Excercise_33_CarpetCostCalculator/src/Floor.java
@@ -0,0 +1,26 @@
+public class Floor {
+
+    private double length;
+    private double width;
+
+    public Floor (double length, double width) {
+        if (length < 0) {
+            this.length = 0;
+        } else {
+            this.length = length;
+        }
+        if (width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+    }
+
+    public double getArea() {
+        return this.length * this.width;
+    }
+
+
+
+
+}
